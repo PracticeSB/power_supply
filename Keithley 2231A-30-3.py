cae22150bef424111d2afa6bca11dc70777b36ed
suny1 <suny1357913@naver.com>
2023-01-05 11:02:59 +0900

    power_supply code

--- Keithley 2231A-30-3.py
+++ Keithley 2231A-30-3.py
@@ -1,5 +1,9 @@
 import pyvisa
 import time
+import datetime
+import keyboard
+
+d = datetime.datetime.now()
 
 rm = 0
 my_PS = 0
@@ -12,15 +16,10 @@ def KEI2231_Connect(rsrcString, getIdStr, timeout, doRst):
     my_PS.StopBits = 1
     if getIdStr == 1:
         print(my_PS.query("*IDN?"))
-        #time.sleep(0.1)
     my_PS.write('SYST:REM')
-    #print(my_PS.timeout)
     my_PS.timeout = timeout
-    #print(my_PS.timeout)
     if doRst == 1:
-        my_PS.write('*RST') #Reset the system
-        #time.sleep(0.1)
-    return my_PS
+        return my_PS
 
 def KEI2231A_Disconnect():
     my_PS.write('SYST:LOC')
@@ -29,29 +28,21 @@ def KEI2231A_Disconnect():
 
 def KEI2231A_SelectChannel(myChan):
     my_PS.write("INST:NSEL %d" % myChan)
-    #time.sleep(0.25)
     return
 
-def KEI2231A_SetVoltage(myV):
-    my_PS.write("VOLT %0.3f" % myV)
-    #time.sleep(0.24)
-    return
+def KEI2231A_SetVoltage(myV,myI):
+    my_PS.write('APPLy CH1,%0.4f %0.4f' %(myV,myI)) #마지막은 current 범위
+    return myV
 
 def KEI2231A_SetCurrent(myI):
     my_PS.write("CURR %f" % myI)
-    #time.sleep(0.24)
     return
 
 def KEI2231A_OutputState(myState):
     if myState == 0:
         my_PS.write("OUTP 0")
-        #time.sleep(0.25)
-        #my_PS.write("OUT:ENAB 0")
     else:
         my_PS.write("OUTP 1")
-        #time.sleep(0.25)
-        #my_PS.write("OUT:ENAB 1")
-    #time.sleep(0.25)
     return
 
 def KEI2231_Send(sndBuffer):
@@ -68,19 +59,29 @@ def KEI2231_Query(sndBuffer):
 
 rm = pyvisa.ResourceManager()   # Opens the resource manager and sets it to variable rm
 
-my_PS = KEI2231_Connect("ASRL5::INSTR", 1, 20000, 1)
-
-
+my_PS = KEI2231_Connect("ASRL5::INSTR", 1, 20000, 1) #VISA communication
 KEI2231A_SelectChannel(1)
-t1 = time.time()    # Capture start time....
-KEI2231A_SetVoltage(10.0)
-KEI2231A_SetCurrent(2.0)
-KEI2231A_OutputState(1)
-t2 = time.time() # Capture stop time...
+KEI2231A_OutputState(1) # 1일 경우에는 ON
+
+while True:
+    if keyboard.is_pressed("a"):
+        break
+    t1 = time.time()    # Capture start time....
+    Input_voltage = 5
+    KEI2231A_SetVoltage(Input_voltage,1)
+    my_PS.write('FETC:CURR?')
+    Current = my_PS.read()
+    Resistance = Input_voltage/float(Current)
+    print('Current value: ',"%0.4f" %float(Current))
+    print('Resistance value:', "%0.4f" %Resistance)
+    t2 = time.time() # Capture stop time...
+    print("Loop time: ","{0:.4f} s".format((t2-t1)))
+
+print(d)
 KEI2231A_OutputState(0)
 KEI2231A_Disconnect()
 
 rm.close
 
 
-print("{0:.4f} s".format((t2-t1)))
+
